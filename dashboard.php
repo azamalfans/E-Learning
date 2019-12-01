65b9e4957d2af973c3ae19e1fef1b2a0436aec33
Ibnu Azam A <azam.alfanibnu@gmail.com>
2019-12-01 13:50:33 +0700

    dfgsdf

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="dashboard.css">
+	<link rel="stylesheet" href="style/dashboard.css">
 	<title>Dashboard</title>
 </head>
 
@@ -17,39 +17,35 @@
 		</div>
 		<p>Username</p>
 		<ul>
-			<a href="index.php?page=course">
+			<a href="dashboard.php?page=course">
 				<li>Course</li>
 			</a>
-			<a href="index.php?page=quiz">
+			<a href="dashboard.php?page=quiz">
 				<li>Quiz</li>
 			</a>
-			<a href="index.php?page=tugas">
+			<a href="dashboard.php?page=tugas">
 				<li>Assignment</li>
 			</a>
-			<a href="index.php?page=raport">
+			<a href="dashboard.php?page=raport">
 				<li>Raport Book</li>
 			</a>
-			<a href="index.php?page=revision_project">
-				<li>Revision</li>
+			<a href="dashboard.php?page=logout">
+				<li>Log Out</li>
 			</a>
 		</ul>
 	</nav>
 	<div class="main-content">
 		<?php
-		define('index', true);
+		define('dashboard', true);
 
 		if (isset($_GET['page'])) {
 			$page = $_GET['page'];
 		} else {
 			$page = "dashboard";
 		}
-
 		require_once("apps.php");
 		require_once $page . ".php";
-
 		?>
 	</div>
-
 </body>
-
 </html>
